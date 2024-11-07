f7c2f8c98abafbabc9a77ecf9a4ccb78dedc5c82
Ryan Randall <ryan@puzzlet.ai>
2024-11-07 11:01:36 -0500

    Fix, updating deps

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,34 @@
 import type { BaseMDXProvidedComponents } from '@puzzlet/templatedx';
+import type { ReactElement, FC, ReactNode } from 'react';
+
+interface IfProps {
+  condition: boolean;
+  children: ReactNode;
+}
+
+interface ForEachProps<T = any> {
+  arr: T[];
+  children: (item: T) => ReactNode;
+}
+
+type ForEachComponent = <T>(props: ForEachProps<T>) => ReactElement | null;
 
 interface ExtractTextProps {
-  children: any;
+  children: ReactNode;
+}
+
+interface Components extends BaseMDXProvidedComponents {
+  User: FC<ExtractTextProps>;
+  Assistant: FC<ExtractTextProps>;
+  System: FC<ExtractTextProps>;
 }
 
 declare global {
-  const myFilter: Filters['lower'];
-
-  interface MDXProvidedComponents extends BaseMDXProvidedComponents {}
+  interface MDXProvidedComponents extends Components {}
 }
+
+export function useMDXComponents(): MDXProvidedComponents {
+  return components
+}
+
 export {};
