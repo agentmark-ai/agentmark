3aa417063fe13030bc2256a316b65878c5e8ee07
Ryan Randall <ryan@puzzlet.ai>
2024-12-03 15:13:02 -0500

    Fix: inference test

---
+++ packages/ollama/CHANGELOG.md
@@ -0,0 +1,13 @@
+# @puzzlet/openai
+
+## 1.1.0
+
+### Minor Changes
+
+- ed786ff: API key support, telemetry
+
+## 1.0.2
+
+### Patch Changes
+
+- 72e5005: Initial changeset bump
---
+++ packages/ollama/src/ollama-chat.ts
@@ -0,0 +1,65 @@
+import type {
+  IPluginAPI,
+  IModelPlugin,
+  InferenceOptions,
+  AgentMarkOutput,
+  AgentMark,
+} from "@puzzlet/agentmark";
+import { createOllama } from 'ollama-ai-provider';
+
+export default class OllamaChatPlugin implements IModelPlugin {
+  provider: string;
+  apiKey: undefined;
+
+  constructor() {
+    this.provider = "ollama";
+  }
+
+  setApiKey() {
+    console.log('*** No-op for now...');
+  }
+
+  serialize(): string {
+    throw new Error('Ollama serialize not implemented for yet. Open a Issue if you need this.');
+  }
+
+  async deserialize(agentMark: AgentMark, api: IPluginAPI): Promise<any> {
+    const { metadata, messages } = agentMark;
+    const { model: modelConfig } = metadata;
+    const completionParamsPromise = new Promise<any>(
+      async (resolve) => {
+        const ollama = createOllama({
+          fetch: async (_, options) => {
+            const requestBody = JSON.parse(options!.body! as string);
+            resolve(requestBody);
+            return new Response();
+          },
+        });
+        const providerModel = ollama(modelConfig.name);
+        try {
+          await api.runInference(modelConfig.settings, providerModel, messages);
+        } catch (e) {}
+      }
+    );
+    const result = await completionParamsPromise;
+    return result;
+  }
+
+  async runInference(
+    agentMark: AgentMark,
+    api: IPluginAPI,
+    options?: InferenceOptions
+  ): Promise<AgentMarkOutput> {
+    const ollama = createOllama({ fetch: api.fetch });
+    const { metadata, messages } = agentMark;
+    const { model: modelConfig } = metadata;
+    const providerModel = ollama(modelConfig.name);
+    const result = await api.runInference(
+      modelConfig.settings,
+      providerModel,
+      messages,
+      options
+    );
+    return result;
+  }
+}
