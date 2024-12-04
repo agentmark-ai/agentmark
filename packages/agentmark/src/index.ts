166f6154c146d7b84f0e512badb4230b85336062
Kashif <kashif.ali@carbonteq.com>
2024-12-04 18:46:11 +0500

    feat: bump all the packages

--- packages/agentmark/src/index.ts
+++ packages/agentmark/src/index.ts
@@ -5,13 +5,13 @@ export {
   getRawConfig,
   getModel,
 } from "./runtime";
-export { load } from "@puzzlet/templatedx";
-export { toFrontMatter } from './utils';
+export { load, parse } from "@puzzlet/templatedx";
+export { toFrontMatter } from "./utils";
 export { PluginAPI } from "./plugin-api";
 import type { IPluginAPI } from "./plugin-api";
 export type { IPluginAPI };
-import './global.d';
+import "./global.d";
 
-export type { IModelPlugin } from './model-plugin';
+export type { IModelPlugin } from "./model-plugin";
 export { ModelPluginRegistry } from "./model-plugin-registry";
-export type { AgentMark, AgentMarkOutput, InferenceOptions } from './types';
+export type { AgentMark, AgentMarkOutput, InferenceOptions } from "./types";
