82ca05768d05f7059a3cf3c1959c08863b7f1105
kashi11 <68692244+kashi11@users.noreply.github.com>
2025-11-19 19:27:41 +0500

    Feat/trace error message (#384)
    * feat: add a status message for failure in trace
    
    * changeset

--- packages/ui-components/src/sections/traces/trace-drawer/span-info/tabs/input-output-tab/input-output-tab.tsx
+++ packages/ui-components/src/sections/traces/trace-drawer/span-info/tabs/input-output-tab/input-output-tab.tsx
@@ -2,9 +2,12 @@ import { useSpanPrompts } from "../hooks/use-span-prompts";
 import { PromptList } from "./prompt-list";
 import { OutputDisplay } from "./output-display";
 import { TabPanel } from "@mui/lab";
+import { Alert } from "@mui/material";
+import { useSpanInfoContext } from "../../span-info-provider";
 
 export const InputOutputTab = () => {
   const { prompts, outputData } = useSpanPrompts();
+  const { span } = useSpanInfoContext();
 
   return (
     <TabPanel
@@ -20,6 +23,11 @@ export const InputOutputTab = () => {
     >
       <PromptList prompts={prompts} />
       <OutputDisplay outputData={outputData} />
+      {span.data.status_message && (
+        <Alert severity="error" sx={{ mt: 2 }}>
+          {span.data.status_message}
+        </Alert>
+      )}
     </TabPanel>
   );
 };
